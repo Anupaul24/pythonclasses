be4382c820b8d7ac8b6e408f5ad3ce1963ab90a0
Anupaul24 <24anupaul@gmail.com>
2021-06-23 10:51:45 +0530

    my first commit in git

---
+++ src/demonotepad.py
@@ -0,0 +1,5 @@
+import pandas as pd
+#a=pd.read_csv('C:/Users/hp/Desktop/data.csv')
+#print(a.to_string())
+b=pd.read_json('C:/Users/hp/Desktop/test.json')
+print(b)
---
+++ src/pdd.py
@@ -0,0 +1,5 @@
+import pandas as pd
+data={ "calories": [450,320,200],
+        "durattion": [40,30,50]}
+my=pd.DataFrame(data)
+print(my)
